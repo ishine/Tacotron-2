ce64a5a0f684f95e5c1518918f0503e1b34358ef
Carlos Fonseca M <carlfm01@hotmail.com>
2019-09-17 04:47:31 -0600

    Update default

--- train.py
+++ train.py
@@ -97,11 +97,11 @@ def main():
 	parser.add_argument('--tacotron_input', default='training_data/train.txt')
 	parser.add_argument('--wavenet_input', default='tacotron_output/gta/map.txt')
 	parser.add_argument('--name', help='Name of logging directory.')
-	parser.add_argument('--model', default='Tacotron-2')
+	parser.add_argument('--model', default='Tacotron')
 	parser.add_argument('--input_dir', default='training_data', help='folder to contain inputs sentences/targets')
 	parser.add_argument('--output_dir', default='output', help='folder to contain synthesized mel spectrograms')
 	parser.add_argument('--mode', default='synthesis', help='mode for synthesis of tacotron after training')
-	parser.add_argument('--GTA', default='True', help='Ground truth aligned synthesis, defaults to True, only considered in Tacotron synthesis mode')
+	parser.add_argument('--GTA', default='False', help='Ground truth aligned synthesis, defaults to True, only considered in Tacotron synthesis mode')
 	parser.add_argument('--restore', type=bool, default=True, help='Set this to False to do a fresh training')
 	parser.add_argument('--summary_interval', type=int, default=250,
 		help='Steps between running summary ops')
@@ -111,7 +111,7 @@ def main():
 		help='Steps between writing checkpoints')
 	parser.add_argument('--eval_interval', type=int, default=5000,
 		help='Steps between eval on test data')
-	parser.add_argument('--tacotron_train_steps', type=int, default=100000, help='total number of tacotron training steps')
+	parser.add_argument('--tacotron_train_steps', type=int, default=200000, help='total number of tacotron training steps')
 	parser.add_argument('--wavenet_train_steps', type=int, default=500000, help='total number of wavenet training steps')
 	parser.add_argument('--tf_log_level', type=int, default=1, help='Tensorflow C++ log level.')
 	parser.add_argument('--slack_url', default=None, help='slack webhook notification destination link')
